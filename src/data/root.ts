a2b68b8561fb5119966ef6a7107e3c8ff5c0f61b
Mark <poussard.mark@gmail.com>
2024-10-19 16:00:27 +0900

    Added scripts

--- src/data/root.ts
+++ src/data/root.ts
@@ -3,14 +3,14 @@ import JsonUtil from "./util/json-util"
 import Type from "./util/type"
 import Pokemon from "./pokemon"
 
-export default class Data {
+export default class Root {
     constructor(
         readonly pokemons : Pokemon[],
     ) {
     }
 
     static fromJSON = (json: any) => {
-        return new Data(
+        return new Root(
             JsonUtil.assert(json.pokemons, arrayOfType(Type.object(Pokemon.fromJSON)))
         )
     }
---
+++ src/script/validate-data.ts
@@ -0,0 +1,5 @@
+import data from '../../public/data.json';
+import Root from '../data/root';
+
+console.log(data)
+console.log(Root.fromJSON(data));
