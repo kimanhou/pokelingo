e30e65b21b69af106a928ceea3995b920a374424
Kim Anh Nguyen <kimanhou@gmail.com>
2024-10-19 15:57:49 +0900

    🚀 Deploy

--- src/components/Home/MemberSelection.tsx
+++ src/components/Home/MemberSelection.tsx
@@ -1,58 +1,47 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
-import TeamMember from "@/components/TeamMember/TeamMember";
 import Button from "@/components/common/Button/Button";
 import { TeamMemberType } from "@/types";
 import { setSelectedMemberInLocalStorage } from "@/ts/localStorageUtils";
 import styles from "./MemberSelection.module.scss";
 
 interface IMemberSelectionProps {
-  teamMembers: TeamMemberType[];
-  back: () => void;
-  onClickMember: (member: TeamMemberType) => void;
-  selectedMember: TeamMemberType | null;
-  roomLink: string;
-  roomId: string;
+    teamMembers: TeamMemberType[];
+    back: () => void;
+    onClickMember: (member: TeamMemberType) => void;
+    selectedMember: TeamMemberType | null;
+    roomLink: string;
+    roomId: string;
 }
 
 const MemberSelection: FC<IMemberSelectionProps> = ({
-  teamMembers,
-  back,
-  onClickMember,
-  selectedMember,
-  roomLink,
-  roomId,
+    teamMembers,
+    back,
+    onClickMember,
+    selectedMember,
+    roomLink,
+    roomId,
 }: IMemberSelectionProps) => {
-  const next = () => {
-    if (selectedMember === null) return;
+    const next = () => {
+        if (selectedMember === null) return;
 
-    setSelectedMemberInLocalStorage({ member: selectedMember, roomId });
-  };
+        setSelectedMemberInLocalStorage({ member: selectedMember, roomId });
+    };
 
-  return (
-    <>
-      <h2>Choose your character</h2>
-      <div className={styles.memberSelectionContainer}>
-        {teamMembers.map((member) => (
-          <TeamMember
-            key={member.name}
-            teamMember={member}
-            onClick={onClickMember}
-            isHomeSelection={true}
-            isSelected={selectedMember === member}
-          />
-        ))}
-      </div>
-      <div className={styles.buttonsContainer}>
-        <Button onClick={back} text="Back" />
-        {selectedMember !== null && (
-          <Link to={roomLink}>
-            <Button onClick={next} text="Next" secondary />
-          </Link>
-        )}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h2>Choose your character</h2>
+            <div className={styles.memberSelectionContainer}></div>
+            <div className={styles.buttonsContainer}>
+                <Button onClick={back} text="Back" />
+                {selectedMember !== null && (
+                    <Link to={roomLink}>
+                        <Button onClick={next} text="Next" secondary />
+                    </Link>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default MemberSelection;
--- src/stories/EditAvatar/EditAvatar.stories.tsx
+++ src/stories/EditAvatar/EditAvatar.stories.tsx
@@ -1,83 +1,73 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import { useArgs } from "@storybook/preview-api";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
-import { WEBCX_TEAM_1 } from "@/data/webcx-1";
 import psyduck from "@/assets/avatars/54.svg";
 import EditAvatar from "@/components/EditAvatar/EditAvatar";
 import SideSheet from "@/components/common/SideSheet/SideSheet";
 import Avatar from "@/components/common/Avatar/Avatar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ToastNotificationEnum } from "@/components/ToastNotification/enum";
+import { Dispatch, SetStateAction } from "react";
 
 const meta: Meta<typeof EditAvatar> = {
-  component: EditAvatar,
+    component: EditAvatar,
 };
 
 export default meta;
 type Story = StoryObj<typeof EditAvatar>;
 
 export const Default: Story = {
-  args: {
-    roomId: "6d630f24-ba2d-400b-b4de-7128b872efb7",
-    memberId: "49d0cd19-1ca0-476c-81f4-efddf43275ef",
-    showToast: (message: string, type: ToastNotificationEnum) =>
-      action("showToast")(message, type),
-    members: WEBCX_TEAM_1.members.flat(),
-    selectedAvatarImageUrl: psyduck,
-  },
-  render: function Render(args) {
-    const [{ isOpen }, updateArgs] = useArgs();
+    args: {},
+    render: function Render(args) {
+        const [{ isOpen }, updateArgs] = useArgs();
 
-    function setIsOpen(value: boolean) {
-      updateArgs({ isOpen: value });
-    }
+        function setIsOpen(value: boolean) {
+            updateArgs({ isOpen: value });
+        }
 
-    return (
-      <>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ position: "relative" }}>
-            <Avatar src={psyduck} backgroundColor="#fcf6bd" />
-            <button
-              onClick={() => setIsOpen(true)}
-              style={{
-                position: "absolute",
-                right: 0,
-                bottom: "-4px",
-                borderRadius: "100%",
-                background: "rgba(255, 255, 255, 0.5)",
-                padding: "4px",
-                // missing hover
-              }}
-            >
-              <FontAwesomeIcon icon={faPencil} color="var(--color-grey)" />
-            </button>
-          </div>
-          <h2>Hi Poney!</h2>
-        </div>
-        <SideSheet
-          isVisible={isOpen}
-          setIsVisible={setIsOpen}
-          transitionFromBottom={true}
-          onEnter={() => {
-            document.body.style.overflow = "hidden";
-          }}
-        >
-          <EditAvatar
-            {...args}
-            closeEditAvatar={() => {
-              document.body.style.overflow = "auto";
-              setIsOpen(false);
-            }}
-          />
-        </SideSheet>
-      </>
-    );
-  },
+        return (
+            <>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <div style={{ position: "relative" }}>
+                        <Avatar src={psyduck} backgroundColor="#fcf6bd" />
+                        <button
+                            onClick={() => setIsOpen(true)}
+                            style={{
+                                position: "absolute",
+                                right: 0,
+                                bottom: "-4px",
+                                borderRadius: "100%",
+                                background: "rgba(255, 255, 255, 0.5)",
+                                padding: "4px",
+                                // missing hover
+                            }}
+                        >
+                            <FontAwesomeIcon
+                                icon={faPencil}
+                                color="var(--color-grey)"
+                            />
+                        </button>
+                    </div>
+                    <h2>Hi Poney!</h2>
+                </div>
+                <SideSheet
+                    isVisible={isOpen}
+                    setIsVisible={
+                        setIsOpen as Dispatch<SetStateAction<boolean>>
+                    }
+                    transitionFromBottom={true}
+                    onEnter={() => {
+                        document.body.style.overflow = "hidden";
+                    }}
+                >
+                    <EditAvatar {...args} />
+                </SideSheet>
+            </>
+        );
+    },
 };
