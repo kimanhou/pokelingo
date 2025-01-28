e027f92721466aa54666591f0bf818bc4f84ea17
Kim Anh Nguyen <kimanhou@gmail.com>
2025-01-28 11:29:53 +0100

    🚧 WIP mobile animation

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,12 +6,23 @@ import logo from "@/assets/logo.svg";
 import learn from "@/assets/study_jigglypuff.png";
 import quiz from "@/assets/think_squirtle.png";
 import { getLastVisit, setLastVisit } from "@/ts/localStorageUtils";
-import { isBeforeToday } from "@/ts/utils";
+import { isBeforeToday, isMobile as isMobileFunc } from "@/ts/utils";
+import { useDeviceType } from "@/hooks/useMedia";
 import styles from "./Home.module.scss";
 
 const Home: FC = (props) => {
+    const deviceType = useDeviceType();
+    const isMobile = isMobileFunc(deviceType);
     const [isExplanationVisible, setIsExplanationVisible] = useState(false);
     const [isExplanationLoaded, setIsExplanationLoaded] = useState(false);
+    const [expandableWidth, setExpandableWidth] = useState(0);
+
+    const triggerMoves = () => {
+        if (isMobile) {
+            setTimeout(() => setExpandableWidth(50), 1000);
+            setTimeout(() => setExpandableWidth(0), 1500);
+        }
+    };
 
     useEffect(() => {
         const lastVisit = getLastVisit();
@@ -24,12 +35,22 @@ const Home: FC = (props) => {
         }
     }, [isExplanationLoaded]);
 
+    useEffect(() => {
+        const lastVisit = getLastVisit();
+        if (lastVisit && !isBeforeToday(new Date(lastVisit))) {
+            triggerMoves();
+        }
+    }, []);
+
     return (
         <div className={styles.home}>
             <img src={logo} className={styles.logo} />
             <div className={styles.optionsContainer}>
                 <div className={styles.optionContainer}>
-                    <div className={styles.placeholder}></div>
+                    <div
+                        className={styles.placeholder}
+                        style={{ width: expandableWidth }}
+                    ></div>
                     <HomeOption
                         to="/learn"
                         text="Learn"
@@ -46,7 +67,10 @@ const Home: FC = (props) => {
                         imageUrl={quiz}
                         textFirst
                     />
-                    <div className={styles.placeholder}></div>
+                    <div
+                        className={styles.placeholder}
+                        style={{ width: expandableWidth }}
+                    ></div>
                 </div>
             </div>
 
@@ -55,6 +79,7 @@ const Home: FC = (props) => {
                 setIsVisible={setIsExplanationVisible}
                 backgroundColor="var(--color-logo-light)"
                 withBackdrop
+                onExit={() => triggerMoves()}
             >
                 <Explanation
                     close={() => setIsExplanationVisible(false)}
