8e907d2b09ffbeb135bbd444252e86d8e082e61a
Kim Anh Nguyen <kimanhou@gmail.com>
2025-03-14 13:53:47 +0100

    🔥 Remove unused

--- src/script/transform-data.ts
+++ src/script/transform-data.ts
@@ -1,39 +1,45 @@
 import * as fs from "fs";
-import inData from "@/data/data.json"
+import inData from "@/data/data.json";
 import Root from "@/model/root/root";
 import Creature from "@/model/creature/creature";
-import { AVATARS } from "@/data/avatars";
 import Word from "@/model/creature/word";
 import Text from "@/model/creature/text";
 import CreatureTag from "@/model/creature/creature-tag";
 
-
 // const previousCreatures = (inData.creatures as any)
-const previousCreatures = inData.creatures // Use this line for compile time check of previousCreatures usage
+const previousCreatures = inData.creatures; // Use this line for compile time check of previousCreatures usage
 
 const creatures = [];
 
-for(let i=0; i<30; i++){
+for (let i = 0; i < 30; i++) {
     creatures.push(
         new Creature(
-            i+1,
-            previousCreatures[i]?.en ?? AVATARS[i].name ?? "",
+            i + 1,
+            previousCreatures[i]?.en ?? "",
             new Word(
                 previousCreatures[i]?.ja?.romaji ?? "",
                 previousCreatures[i]?.ja?.kana ?? "",
-                previousCreatures[i]?.ja?.kanji ?? undefined,
+                previousCreatures[i]?.ja?.kanji ?? undefined
             ),
-            previousCreatures[i]?.imageUrl ?? `/assets/creature/${i+1}.svg`,
-            previousCreatures[i]?.tags?.map(CreatureTag.fromJSON) ?? [CreatureTag.GEN1],
-            previousCreatures[i]?.types ?? AVATARS[i].types ?? [],
+            previousCreatures[i]?.imageUrl ?? `/assets/creature/${i + 1}.svg`,
+            previousCreatures[i]?.tags?.map(CreatureTag.fromJSON) ?? [
+                CreatureTag.GEN1,
+            ],
+            previousCreatures[i]?.types ?? [],
             new Text(
                 previousCreatures[i]?.origin?.value ?? "",
-                previousCreatures[i]?.origin?.words?.map((x: any) => new Word(x.romaji, x.kana, x.kanji)) ?? []
+                previousCreatures[i]?.origin?.words?.map(
+                    (x: any) => new Word(x.romaji, x.kana, x.kanji)
+                ) ?? []
             ),
-            previousCreatures[i]?.originTags ?? [],
+            previousCreatures[i]?.originTags ?? []
         )
-    )
+    );
 }
 const outData = new Root(creatures);
 
-fs.writeFileSync("./script-out/generate-empty-out.json", JSON.stringify(outData), {flag: "w"})
+fs.writeFileSync(
+    "./script-out/generate-empty-out.json",
+    JSON.stringify(outData),
+    { flag: "w" }
+);
